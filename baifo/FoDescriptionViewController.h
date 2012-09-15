f7829c18689ea53bc8512501a678b01f5329d0a4
Liming Hong <hong_liming@163.com>
2012-09-16 04:10:20 +0800

    show disabled model

---
+++ baifo/FoDescriptionViewController.h
@@ -0,0 +1,13 @@
+//
+//  FoDescriptionViewController.h
+//  baifo
+//
+//  Created by Hong Liming on 9/16/12.
+//  Copyright (c) 2012 Hong Liming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FoDescriptionViewController : UIViewController
+
+@end
---
+++ baifo/FoDescriptionViewController.m
@@ -0,0 +1,43 @@
+//
+//  FoDescriptionViewController.m
+//  baifo
+//
+//  Created by Hong Liming on 9/16/12.
+//  Copyright (c) 2012 Hong Liming. All rights reserved.
+//
+
+#import "FoDescriptionViewController.h"
+
+@interface FoDescriptionViewController ()
+
+@end
+
+@implementation FoDescriptionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
